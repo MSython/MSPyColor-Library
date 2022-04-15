c706b393f6006cea3389766d15283ac6a765cf7f
M.Sython <103525051+MSython@users.noreply.github.com>
2022-04-15 21:00:29 +0430

    Add files via upload

---
+++ MSyCo.py
@@ -0,0 +1,28 @@
+class color : 
+   green = '\033[92m'
+   red = '\033[91m'
+   white = '\033[0m'
+   yellow = '\033[93m'
+   blue = '\033[94m'
+   purple = '\033[95m'
+   cyan = '\033[96m'
+   gray = '\033[90m'
+
+def pgreen(text):
+   print(color.green + str(text))
+def pred(text):
+   print(color.red + str(text))
+def pyellow(text):
+   print(color.yellow + str(text))
+def pblue(text):
+   print(color.blue + str(text))
+def ppurple(text):
+   print(color.purple + str(text))
+def pcyan(text):
+   print(color.cyan + str(text))
+def pgray(text):
+   print(color.gray + str(text))
+def pgreen(text):
+   print(color.green + str(text))
+def pwhite(text):
+   print(color.white + str(text))
